d8a3ee413381ee82bc85145be090ed4a36275a9e
Ibnu Jakaria <ibnujakaria@outlook.com>
2019-08-05 15:45:06 +0700

    filter by keyword

--- src/components/DropdownSelect.js
+++ src/components/DropdownSelect.js
@@ -4,6 +4,7 @@ class DropdownSelect {
     this.$dropdownSelect = null
     this.$input = null
     this.$optionItems = []
+    this.keyword = null
 
     this._buildDropdownSelect()
     this._buildSearchInput()
@@ -27,6 +28,12 @@ class DropdownSelect {
     this.$input.classList.add('form-control')
     this.$input.setAttribute('placeholder', 'Search')
     this.$input.setAttribute('type', 'text')
+    this.$input.addEventListener('keyup', (e) => {
+      this.keyword = this.$input.value
+      this._buildOptionItems()
+      this._rerenderOptionsItems()
+    })
+    this.$input.focus()
 
     let inputContainer = document.createElement('div')
     inputContainer.classList.add('p-2')
@@ -37,14 +44,23 @@ class DropdownSelect {
 
   _buildOptionItems () {
     let store = this.$container.$root.$store
+    let listGroup = this.$dropdownSelect.querySelector('.list-group')
+    
     this.$optionItems = []
+    
+    if (!listGroup) {
+      listGroup = document.createElement('ul')
+      listGroup.classList.add('list-group', 'mt-0')
+  
+      this.$dropdownSelect.appendChild(listGroup)
+    }
 
-    let listGroup = document.createElement('ul')
-    listGroup.classList.add('list-group', 'mt-0')
+    // clear option
+    while (listGroup.lastChild) {
+      listGroup.removeChild(listGroup.lastChild)
+    }
 
-    this.$dropdownSelect.appendChild(listGroup)
-
-    store.items.forEach(item => {
+    this.filteredItems.forEach(item => {
       let itemDom = document.createElement('li')
       itemDom.classList.add(
         'list-group-item', 'd-flex', 'flex-row',
@@ -73,17 +89,40 @@ class DropdownSelect {
       this.$optionItems.push(itemDom)
       listGroup.appendChild(itemDom)
     })
+
+    // if no result
+    if (this.filteredItems.length < 1) {
+      let itemDom = document.createElement('li')
+      itemDom.classList.add(
+        'list-group-item', 'd-flex', 'flex-row',
+        'justify-content-between', 'p-2', 'rounded-0',
+        'list-group-item-secondary'
+      )
+      itemDom.innerText = 'No items.'
+      listGroup.appendChild(itemDom)
+    }
   }
 
   _rerenderOptionsItems () {
     let store = this.$container.$root.$store
 
     this.$optionItems.forEach(itemDom => {
+      // is selected
       if (store.selectedItems.find(item => item.value === itemDom.getAttribute('value'))) {
         itemDom.classList.add('list-group-item-primary')
       } else {
         itemDom.classList.remove('list-group-item-primary')
       }
+    })
+  }
+
+  get filteredItems () {
+    return this.$container.$root.$store.items.filter(item => {
+      if (this.keyword) {
+        return item.label.toLowerCase().includes(this.keyword.toLowerCase())
+      }
+
+      return true
     })
   }
 }
